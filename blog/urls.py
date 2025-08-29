4b6df8de74f6e91796a1602e56b2b1c34a2109a7
Gabriel <gabriel@mariana-hp-prodesk-600-g1-sff>
2025-08-29 15:28:45 -0300

    Atualizacao do site

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from . import views
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
 urlpatterns = [
    path ('blog/', views.post_list, name= 'post_list'), 
 
 ]   
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
